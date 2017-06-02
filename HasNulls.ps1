bb7a29cdbc9b345d791e1a642aca0f9c28723194
John Lusk <john-github@how-hard-can-it-be.com>
2017-06-02 12:25:39 -0400

    First attempt at adding pipeline capability -- not right yet.

--- HasNulls.ps1
+++ HasNulls.ps1
@@ -12,14 +12,27 @@
 	ls -rec | ? {[IO.FileInfo].IsAssignableFrom($_.GetType()) -and (-not (HasNulls $_ ))} | ss -list '\bEDTab\b' | ogv
 #>
 function HasNulls
-    (
+(
+    [string]
 	# File to check
-	$file,
+	$filename,
+
+    [System.IO.FileInfo]
+    # A File object
+    $inputFileObject,
+
+    [System.IO.DirectoryInfo]
+    # A Directory object. Causes return value to be the value of -nonfileResult (q.v.)
+    $inputDirectoryObject,
 	
 	[boolean]
 	# The result to return if the input is not a file (or string filename)
-	$nonfileResult
-	)
+	$nonfileResult,
+
+    [switch]
+    # Reverse the sense of the return value so this function can be used as a filter to allow only text files through
+    $not
+)
 
 {
 
@@ -31,31 +44,36 @@ function HasNulls
 #         )
     Process
     {
-        if ($file -eq $Null) { $file = $_ }
+        # if ($filename -eq $Null) { $filename = $_ } # I expect parameter-matching to correctly assign the param, making
+                                                      # this unneded (and incorrect).
 	    try 
 	    {
-            $ftype = $file.GetType().ToString()
-		    if ($ftype -eq "System.String")
+#            $ftype = $filename.GetType().ToString()
+#		    if ($ftype -eq "System.String")
+            if (($filename -ne $Null) -and ($filename -ne ""))
 		    {
-			    Write-Debug "arg is string"
-			    $fn = ((Get-Location).Path, $file) -join "\"
+			    Write-Debug ("arg is string ({0})" -f $filename)
+			    $fn = ((Get-Location).Path, $filename) -join "\"
 			    Write-Debug "Opening `"$fn`""
 			    $reader = [IO.File]::OpenRead( $fn)
 		    }
-		    elseif ($ftype -eq "System.IO.FileInfo")
+		    # elseif ($ftype -eq "System.IO.FileInfo")
+            elseif ($inputFileObject -ne $Null)
 		    {
 			    Write-Debug "arg is fileinfo"
 			    $reader = $file.OpenRead()
 		    }
-            elseif ($ftype -eq "System.IO.DirectoryInfo")
+            # elseif ($ftype -eq "System.IO.DirectoryInfo")
+            elseif ($inputDirectoryObject -ne $Null)
             {
                 Write-Debug ('{0} is directory' -f $file.FullName)
                 return $nonFileResult
             }
 			else
 			{
-                Write-Debug ('arg name, type: {0}, {1}' -f $file.ToString(),$ftype)
-				return $nonfileResult
+                # Write-Debug ('arg name, type: {0}, {1}' -f $file.ToString(),$ftype)
+                Write-Error ('No arguments supplied')
+				return $nonfileResult             # throw instead?
 			}
 # 		    else
 # 		    {
